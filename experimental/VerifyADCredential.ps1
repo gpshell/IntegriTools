f208acb22fd70e113ba54bf2e28191b8e4b64507
gpshell <george.power@integrity.ky>
2026-07-17 09:11:50 -0500

    Create VerifyADCredential.ps1

---
+++ experimental/VerifyADCredential.ps1
@@ -0,0 +1,9 @@
+### Opens a username/password entry field and returns "true" if the credentials are correct
+### For best results, run this script on the domain controller
+### Working as of: 2026-07-17
+$creds=Get-Credential
+Function Test-ADCreds {
+    param($username, $password)
+    (New-Object DirectoryServices.DirectoryEntry "",$username,$password).psbase.name -ne $null
+}
+Test-ADCreds -username $creds.UserName -password $creds.GetNetworkCredential().password
